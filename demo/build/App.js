c9433d703e49c3e7f87715e6802e06db0d3c3e83
Lưu Minh Hoàng <ceriagame@gmail.com>
2020-10-18 10:18:12 +0700

    Demo: Two-way binding for animationTimingFunc input

--- demo/build/App.js
+++ demo/build/App.js
@@ -6,8 +6,6 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var e = React.createElement;
-
 var App = function (_React$Component) {
   _inherits(App, _React$Component);
 
@@ -16,83 +14,140 @@ var App = function (_React$Component) {
 
     var _this = _possibleConstructorReturn(this, (App.__proto__ || Object.getPrototypeOf(App)).call(this, props));
 
-    _this.state = { liked: false };
+    _this.state = {
+      glide: null,
+      animationTimingFunc: ""
+    };
+
+    _this.state = {
+      animationTimingFunc: "$$bounce"
+    };
     return _this;
   }
 
   _createClass(App, [{
+    key: "componentDidMount",
+    value: function componentDidMount() {
+      var g = this.glideFactory({
+        animationTimingFunc: this.state.animationTimingFunc
+      });
+      g.mount();
+      this.setState({ glide: g });
+    }
+  }, {
+    key: "componentDidUpdate",
+    value: function componentDidUpdate() {
+      if (!!this.state.glide && !!this.state.glide.update) {
+        // check if MOUNTED
+        this.state.glide.update({
+          animationTimingFunc: this.state.animationTimingFunc
+        });
+      }
+    }
+  }, {
     key: "render",
     value: function render() {
+      var _this2 = this;
+
       if (this.state.liked) {
         return "You liked this.";
       }
 
       return React.createElement(
-        "div",
-        { className: "glide" },
+        "content",
+        null,
+        React.createElement("input", {
+          type: "text",
+          value: this.state.animationTimingFunc,
+          onChange: function onChange(e) {
+            return _this2.setState({ animationTimingFunc: e.target.value });
+          }
+        }),
         React.createElement(
           "div",
-          { className: "glide__track", "data-glide-el": "track" },
+          { className: "glide" },
           React.createElement(
-            "ul",
-            { className: "glide__slides" },
+            "div",
+            { className: "glide__track", "data-glide-el": "track" },
             React.createElement(
-              "li",
-              { className: "glide__slide" },
-              React.createElement("img", { src: "./img/img01.jpg", alt: "01", height: "500" })
-            ),
-            React.createElement(
-              "li",
-              { className: "glide__slide" },
-              React.createElement("img", { src: "./img/img02.jpg", alt: "02", height: "500" })
-            ),
-            React.createElement(
-              "li",
-              { className: "glide__slide" },
-              React.createElement("img", { src: "./img/img03.jpg", alt: "03", height: "500" })
-            ),
-            React.createElement(
-              "li",
-              { className: "glide__slide" },
-              React.createElement("img", { src: "./img/img04.jpg", alt: "04", height: "500" })
-            ),
-            React.createElement(
-              "li",
-              { className: "glide__slide" },
-              React.createElement("img", { src: "./img/img05.jpg", alt: "05", height: "500" })
+              "ul",
+              { className: "glide__slides" },
+              React.createElement(
+                "li",
+                { className: "glide__slide" },
+                React.createElement("img", { src: "./img/img01.jpg", alt: "01", height: "500" })
+              ),
+              React.createElement(
+                "li",
+                { className: "glide__slide" },
+                React.createElement("img", { src: "./img/img02.jpg", alt: "02", height: "500" })
+              ),
+              React.createElement(
+                "li",
+                { className: "glide__slide" },
+                React.createElement("img", { src: "./img/img03.jpg", alt: "03", height: "500" })
+              ),
+              React.createElement(
+                "li",
+                { className: "glide__slide" },
+                React.createElement("img", { src: "./img/img04.jpg", alt: "04", height: "500" })
+              ),
+              React.createElement(
+                "li",
+                { className: "glide__slide" },
+                React.createElement("img", { src: "./img/img05.jpg", alt: "05", height: "500" })
+              )
             )
-          )
-        ),
-        React.createElement(
-          "div",
-          { className: "glide__arrows", "data-glide-el": "controls" },
-          React.createElement(
-            "button",
-            {
-              className: "glide__arrow glide__arrow--left",
-              "data-glide-dir": "<"
-            },
-            "prev"
           ),
           React.createElement(
-            "button",
-            {
-              className: "glide__arrow glide__arrow--right",
-              "data-glide-dir": ">"
-            },
-            "next"
+            "div",
+            { className: "glide__arrows", "data-glide-el": "controls" },
+            React.createElement(
+              "button",
+              {
+                className: "glide__arrow glide__arrow--left",
+                "data-glide-dir": "<"
+              },
+              "prev"
+            ),
+            React.createElement(
+              "button",
+              {
+                className: "glide__arrow glide__arrow--right",
+                "data-glide-dir": ">"
+              },
+              "next"
+            )
+          ),
+          React.createElement(
+            "div",
+            { className: "glide__bullets", "data-glide-el": "controls[nav]" },
+            React.createElement("button", { className: "glide__bullet", "data-glide-dir": "=0" }),
+            React.createElement("button", { className: "glide__bullet", "data-glide-dir": "=1" }),
+            React.createElement("button", { className: "glide__bullet", "data-glide-dir": "=2" }),
+            React.createElement("button", { className: "glide__bullet", "data-glide-dir": "=3" }),
+            React.createElement("button", { className: "glide__bullet", "data-glide-dir": "=4" })
           )
-        ),
-        React.createElement(
-          "div",
-          { className: "glide__bullets", "data-glide-el": "controls[nav]" },
-          React.createElement("button", { className: "glide__bullet", "data-glide-dir": "=0" }),
-          React.createElement("button", { className: "glide__bullet", "data-glide-dir": "=1" }),
-          React.createElement("button", { className: "glide__bullet", "data-glide-dir": "=2" }),
-          React.createElement("button", { className: "glide__bullet", "data-glide-dir": "=3" }),
-          React.createElement("button", { className: "glide__bullet", "data-glide-dir": "=4" })
         )
       );
+    }
+
+    // Methods
+
+  }, {
+    key: "glideFactory",
+    value: function glideFactory(options) {
+      var animationTimingFunc = options.animationTimingFunc;
+
+
+      var glide = new Glide(".glide", {
+        type: "carousel",
+        startAt: 0,
+        focusAt: "center",
+        animationTimingFunc: animationTimingFunc,
+        perView: 3
+      });
+      return glide;
     }
   }]);
 
@@ -100,4 +155,4 @@ var App = function (_React$Component) {
 }(React.Component);
 
 var domContainer = document.querySelector("#root");
-ReactDOM.render(e(App), domContainer);
+ReactDOM.render(React.createElement(App), domContainer);
--- demo/src/App.js
+++ demo/src/App.js
@@ -1,9 +1,31 @@
-const e = React.createElement;
-
 class App extends React.Component {
+  state = {
+    glide: null,
+    animationTimingFunc: "",
+  };
+
   constructor(props) {
     super(props);
-    this.state = { liked: false };
+    this.state = {
+      animationTimingFunc: "$$bounce",
+    };
+  }
+
+  componentDidMount() {
+    const g = this.glideFactory({
+      animationTimingFunc: this.state.animationTimingFunc,
+    });
+    g.mount();
+    this.setState({ glide: g });
+  }
+
+  componentDidUpdate() {
+    if (!!this.state.glide && !!this.state.glide.update) {
+      // check if MOUNTED
+      this.state.glide.update({
+        animationTimingFunc: this.state.animationTimingFunc,
+      });
+    }
   }
 
   render() {
@@ -12,51 +34,76 @@ class App extends React.Component {
     }
 
     return (
-      <div className="glide">
-        <div className="glide__track" data-glide-el="track">
-          <ul className="glide__slides">
-            <li className="glide__slide">
-              <img src="./img/img01.jpg" alt="01" height="500"></img>
-            </li>
-            <li className="glide__slide">
-              <img src="./img/img02.jpg" alt="02" height="500"></img>
-            </li>
-            <li className="glide__slide">
-              <img src="./img/img03.jpg" alt="03" height="500"></img>
-            </li>
-            <li className="glide__slide">
-              <img src="./img/img04.jpg" alt="04" height="500"></img>
-            </li>
-            <li className="glide__slide">
-              <img src="./img/img05.jpg" alt="05" height="500"></img>
-            </li>
-          </ul>
+      <content>
+        <input
+          type="text"
+          value={this.state.animationTimingFunc}
+          onChange={(e) =>
+            this.setState({ animationTimingFunc: e.target.value })
+          }
+        ></input>
+
+        <div className="glide">
+          <div className="glide__track" data-glide-el="track">
+            <ul className="glide__slides">
+              <li className="glide__slide">
+                <img src="./img/img01.jpg" alt="01" height="500"></img>
+              </li>
+              <li className="glide__slide">
+                <img src="./img/img02.jpg" alt="02" height="500"></img>
+              </li>
+              <li className="glide__slide">
+                <img src="./img/img03.jpg" alt="03" height="500"></img>
+              </li>
+              <li className="glide__slide">
+                <img src="./img/img04.jpg" alt="04" height="500"></img>
+              </li>
+              <li className="glide__slide">
+                <img src="./img/img05.jpg" alt="05" height="500"></img>
+              </li>
+            </ul>
+          </div>
+          <div className="glide__arrows" data-glide-el="controls">
+            <button
+              className="glide__arrow glide__arrow--left"
+              data-glide-dir="<"
+            >
+              prev
+            </button>
+            <button
+              className="glide__arrow glide__arrow--right"
+              data-glide-dir=">"
+            >
+              next
+            </button>
+          </div>
+          <div className="glide__bullets" data-glide-el="controls[nav]">
+            <button className="glide__bullet" data-glide-dir="=0"></button>
+            <button className="glide__bullet" data-glide-dir="=1"></button>
+            <button className="glide__bullet" data-glide-dir="=2"></button>
+            <button className="glide__bullet" data-glide-dir="=3"></button>
+            <button className="glide__bullet" data-glide-dir="=4"></button>
+          </div>
         </div>
-        <div className="glide__arrows" data-glide-el="controls">
-          <button
-            className="glide__arrow glide__arrow--left"
-            data-glide-dir="<"
-          >
-            prev
-          </button>
-          <button
-            className="glide__arrow glide__arrow--right"
-            data-glide-dir=">"
-          >
-            next
-          </button>
-        </div>
-        <div className="glide__bullets" data-glide-el="controls[nav]">
-          <button className="glide__bullet" data-glide-dir="=0"></button>
-          <button className="glide__bullet" data-glide-dir="=1"></button>
-          <button className="glide__bullet" data-glide-dir="=2"></button>
-          <button className="glide__bullet" data-glide-dir="=3"></button>
-          <button className="glide__bullet" data-glide-dir="=4"></button>
-        </div>
-      </div>
+      </content>
     );
+  }
+
+  // Methods
+
+  glideFactory(options) {
+    const { animationTimingFunc } = options;
+
+    const glide = new Glide(".glide", {
+      type: "carousel",
+      startAt: 0,
+      focusAt: "center",
+      animationTimingFunc,
+      perView: 3,
+    });
+    return glide;
   }
 }
 
 const domContainer = document.querySelector("#root");
-ReactDOM.render(e(App), domContainer);
+ReactDOM.render(React.createElement(App), domContainer);
